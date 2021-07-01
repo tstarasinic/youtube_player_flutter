85a6c4ce872321891f382be34489e627921d2541
tstarasinic <tomaz.starasinic@dhimahi.com>
2021-07-01 09:29:31 +0200

    comment

--- packages/youtube_player_iframe/lib/src/players/youtube_player_mobile.dart
+++ packages/youtube_player_iframe/lib/src/players/youtube_player_mobile.dart
@@ -1,6 +1,7 @@
 // Copyright 2020 Sarbagya Dhaubanjar. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+//Tomaž code fork
 
 import 'dart:async';
 import 'dart:developer';
